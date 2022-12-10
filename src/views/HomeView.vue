b10ca2381e2e3220567f5211d33641ff6521cfa6
BetterChinglish <2199840669@qq.com>
2022-12-10 10:10:51 +0800

    set alias

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img :src="imgsrc" alt="">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from 'components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      imgsrc: require('assets/images/2.png'),
+    }
   }
 }
 </script>
