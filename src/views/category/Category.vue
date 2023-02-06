e19c09cff8a49ec1cbec60e98eb3bba1f663c4a6
BetterChinglish <2199840669@qq.com>
2023-02-07 02:59:09 +0800

    商品分类侧边栏

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -12,27 +12,111 @@
 
       </template> -->
 
-
-
     </nav-bar>
-    <van-button type="primary" />
-    
+
+    <div id="mainbox">
+        <div class="ordertab">
+
+        </div>
+
+        <van-sidebar v-model="active" class="leftmenu">
+            <van-collapse v-model="activeName" accordion>
+                <van-collapse-item 
+                    v-for="category in categories" 
+                    :key="category.id"
+                    :title="category.name" 
+                    :name="category.name">
+
+                    <van-sidebar-item 
+                        v-for="sub in category.children"
+                        :key="sub.id"
+                        :title="sub.name" />
+
+                </van-collapse-item>
+            </van-collapse>
+        </van-sidebar>
+
+        <div class="goodslist">
+            
+        </div>
+
+    </div>
 
 </div>
 </template>
 
 <script>
 import NavBar from '@/components/common/navbar/NavBar.vue';
+import {ref, reactive, onMounted} from 'vue';
+import {getCategory} from 'network/category';
 
 export default {
     name: 'Category',
     components: {
         NavBar,
-    }
+    },
+    setup() {
+        // 左侧边栏当前选择第几个
+        let active = ref(0);
+        // 侧边栏一级选项
+        let activeName = ref(1);
+
+        // 分类有哪些
+        let categories = ref([]);
+
+        onMounted(()=>{
+            getCategory().then(res=>{
+                // 由于数据库被其他人添加了别的数据, 这里只使用前5个
+                categories.value = res.categories.slice(0,5);
+                console.log(categories);
+            })
+        })
+        
+
+        return {
+            active,
+            categories,
+            activeName,
+        }
+    },
     
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+#mainbox {
+    margin-top: 45px;
+    display: flex;
+    .ordertab {
+        flex: 1;
+        flex: right;
+        width: 100%;
+        height: 50px;
+        z-index: 9;
+        position: fixed;
+        top: 45px;
+        right: 0;
+        left: 130px;
+    }
+
+    .leftmenu {
+
+        position: fixed;
+        top: 110px;
+        left: 0;
+        width: 130px;
+        height: 75vh;
+    }
+
+    .goodslist {
+        flex: 1;
+        height: 100vh;
+        position: absolute;
+        top: 110px;
+        left: 130px;
+        right: 0;
+
+    }
+}
 
 </style>
